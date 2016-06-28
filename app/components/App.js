30bb9a4b4e9eb1424f04afb1dce4d4c238ad29b8
stevet14 <steve.taplin@gmail.com>
2016-06-28 23:54:30 +0100

    Footer added

--- app/components/App.js
+++ app/components/App.js
@@ -2,12 +2,14 @@
  * Created by stevet on 28/06/2016.
  */
 import React from 'react';
+import Footer from './Footer';
 
 class App extends React.Component {
     render() {
         return (
             <div>
                 {this.props.children}
+                <Footer />
             </div>
         );
     }

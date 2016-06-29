a1e97a6a5b28cf14611d3b77ad15cab3a881da7e
stevet14 <steve.taplin@gmail.com>
2016-06-29 17:42:12 +0100

    React versioning fixed

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -2,30 +2,8 @@
  * Created by stevet on 28/06/2016.
  */
 import React from 'react';
-import {Link} from 'react-router';
-//import FooterStore from '../stores/FooterStore'
-//import FooterActions from '../actions/FooterActions';
 
 class Footer extends React.Component {
-    constructor(props) {
-        super(props);
-//        this.state = FooterStore.getState();
-//        this.onChange = this.onChange.bind(this);
-    }
-
-    componentDidMount() {
-//        FooterStore.listen(this.onChange);
-//        FooterActions.getTopCharacters();
-    }
-
-    componentWillUnmount() {
-//        FooterStore.unlisten(this.onChange);
-    }
-
-    onChange(state) {
-//        this.setState(state);
-    }
-
     render() {
         return (
             <footer>
